24f421223dee1b7daf327441f9ed1ead0b4e3ddd
Lokesh <lokeshkoli050138@gmail.com>
2024-09-25 22:26:01 -0700

    indian map

--- assets/js/script.js
+++ assets/js/script.js
@@ -260,3 +260,96 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
  
+
+// map Start 
+
+FusionCharts.ready(function() {
+  var demoMap = new FusionCharts({
+      type: "maps/india",
+      renderAt: "indian-map",
+      height: "650",
+      width: "100%",
+      dataFormat: "json",
+      dataSource: {
+          "chart": {
+              "subCaption": "Census 2024",
+              "captionFontSize": "25",
+              "captionFontBold": "0",
+              "subCaptionFontSize": "18",
+              "subCaptionFontBold": "0",
+              "canvasPadding": "30",
+              "showBorder": "0",
+              "showCanvasBorder": "0",
+              "legendBorderAlpha": "0",
+              "showLegend": "0",
+              "toolTipPadding": "9",
+              "toolTipColor": "#FFFFFF",
+              "toolTipBgColor": "#000000",
+              "toolTipBgAlpha": "70",
+              "toolTipBorderThickness": "1",
+              "tooltipBorderRadius": "3",
+              "toolTipBorderColor": "#FFFFFF",
+              "toolTipBorderAlpha": "80",
+              "showToolTipShadow": "0",
+              "baseFont": "Lato"
+          },
+          
+          "colorrange": {
+              "minvalue": "300",
+              "startlabel": "Low",
+              "endlabel": "High",
+              "code": "#efedf5",
+              "gradient": "1",
+              "color": [{
+                  "maxvalue": "220000",
+                  "displayvalue": "Avg.",
+                  "code": "#bcbddc"
+              }, {
+                  "maxvalue": "1400000",
+                  "code": "#756bb1"
+              }]
+          },
+          
+          "data": [
+              { "id": "015", "value": "58438" },
+              { "id": "014", "value": "41344" },
+              { "id": "028", "value": "292124" },
+              { "id": "006", "value": "44191" },
+              { "id": "034", "value": "63906" },
+              { "id": "013", "value": "250052" },
+              { "id": "010", "value": "587935" },
+              { "id": "029", "value": "226463" },
+              { "id": "033", "value": "625561" },
+              { "id": "005", "value": "170466" },
+              { "id": "030", "value": "4228" },
+              { "id": "003", "value": "5232" },
+              { "id": "025", "value": "6799" },
+              { "id": "022", "value": "10650" },
+              { "id": "024", "value": "5527" },
+              { "id": "032", "value": "8428" },
+              { "id": "023", "value": "8074" },
+              { "id": "004", "value": "101877" },
+              { "id": "035", "value": "441481" },
+              { "id": "016", "value": "92724" },
+              { "id": "026", "value": "135255" },
+              { "id": "007", "value": "67474" },
+              { "id": "020", "value": "209546" },
+              { "id": "012", "value": "377633" },
+              { "id": "009", "value": "1691" },
+              { "id": "008", "value": "2046" },
+              { "id": "021", "value": "1382174" },
+              { "id": "002", "value": "546638" },
+              { "id": "017", "value": "632636" },
+              { "id": "011", "value": "40997" },
+              { "id": "019", "value": "332" },
+              { "id": "018", "value": "486131" },
+              { "id": "031", "value": "776706" },
+              { "id": "027", "value": "18077" },
+              { "id": "001", "value": "3268" },
+              { "id": "036", "value": "546638" }
+          ]
+      }
+  }).render();	
+});
+
+// map End 
